2c989e52642fd894966ca126991767d110605cba
ssx <s95175379@gmail.com>
2021-11-04 19:31:28 +0800

    first

---
+++ ch2_9.java
@@ -0,0 +1,12 @@
+
+public class ch2_9 {
+	public static void main(String[] args)
+	{
+		for (int i = 1;i <= 5; i++) {
+			System.out.println("第" + i + "次的迴圈");
+		}
+		
+		System.out.println("迴圈結束");
+	}
+}
+//108360136
